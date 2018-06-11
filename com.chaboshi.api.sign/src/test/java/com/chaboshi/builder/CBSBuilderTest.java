6ba0841914648a6f534d697d693db4348d63d8f8
sleepMushroom <sleepSwamm@gmail.com>
2018-06-11 15:05:00 +0800

    新调用方式单元测试

---
+++ com.chaboshi.api.sign/src/test/java/com/chaboshi/builder/CBSBuilderTest.java
@@ -0,0 +1,21 @@
+package com.chaboshi.builder;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import java.util.HashMap;
+
+public class CBSBuilderTest {
+
+	@Test
+	public void newCBSBuilder() {
+		String userId = "146574";
+		String key = "a81cd13aa735140fd72c7ab402a2d98f";
+		CBSBuilder cbsBuilder = CBSBuilder.newCBSBuilder(userId, key, true);
+		HashMap<String, Object> params = new HashMap<String, Object>();
+		params.put("tradeno", "1002461834566983680");
+		String s = cbsBuilder.sendPost("/insurance/orderInfo", params);
+		System.out.println(s);
+	}
+
+}
