375c4695ad8ca343d2fbafe6904b6fadabbaa922
rongyaoli <584420909@qq.com>
2019-02-28 20:01:21 +0800

    修改签名测试地址和其他签名逻辑

--- com.chaboshi.api.sign.detection/src/main/java/com/chaboshi/api/sign/detection/builder/CBSBuilder.java
+++ com.chaboshi.api.sign.detection/src/main/java/com/chaboshi/api/sign/detection/builder/CBSBuilder.java
@@ -5,6 +5,7 @@ import com.chaboshi.api.sign.detection.http.HttpRequest;
 import com.chaboshi.api.sign.detection.signutil.SignUtil;
 import org.apache.commons.lang3.StringUtils;
 
+import java.net.URLEncoder;
 import java.util.HashMap;
 import java.util.UUID;
 
@@ -15,7 +16,7 @@ import java.util.UUID;
  ****************************************
  */
 public class CBSBuilder {
-	private final String CBS_TEST = "http://tapi.chaboshi.cn";
+	private final String CBS_TEST = "http://jxapi.chaboshi.cn";
 	private final String CBS_ONLINE = "https://api.chaboshi.cn";
 
 	/**
@@ -147,6 +148,6 @@ public class CBSBuilder {
 		sb.append("&").append(CBSField.NONCE).append("=").append(nonce);
 		String signature = SignUtil.getSignature(keySecret, sb.toString());
 		sb.append("&").append(CBSField.SIGNATURE).append("=").append(signature);
-		return sb.toString();
+		return URLEncoder.encode(sb.toString(), "UTF-8");
 	}
 }
